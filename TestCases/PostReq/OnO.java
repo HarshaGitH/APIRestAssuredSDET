a29b985a640b5add9288f41a765c2798e7717d59
Harshavardhan Reddy <Harshavardhan.Reddy@Mercator.com>
2019-07-21 14:21:32 +0400

    Test

--- TestCases/PostReq/OnO.java
+++ TestCases/PostReq/OnO.java
@@ -1,11 +1,8 @@
 package PostReq;
 
-import org.json.simple.JSONObject;
-import org.testng.annotations.Test;
 
+import org.testng.annotations.Test;
 import io.restassured.RestAssured;
-import io.restassured.config.EncoderConfig;
-import io.restassured.http.ContentType;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -14,26 +11,26 @@ public class OnO {
 	
 	@Test
 	public void bearer() {
-		
-		
 		//Base uri
 		RestAssured.baseURI = "http://10.4.11.108:8090/api/iam/oauth";
-		
-	    RequestSpecification request = RestAssured.given().config(RestAssured.config() .encoderConfig(EncoderConfig.encoderConfig() 
-	    		.encodeContentTypeAs("x-www-form-urlencoded", ContentType.URLENC))) .contentType(ContentType.URLENC.withCharset("UTF-8")) 
-			      .formParam("grant_type", "password")
-			      .formParam("username", "HONO")
-			      .formParam("password", "PASSWORD%")
-			      .header("Content-Type", "application/x-www-form-urlencoded")
-			     .header("Authorization", "Basic bWVyY2F0b3I6")
-			     .header("Referer", "http://10.4.11.108").request();
-	    
+		RequestSpecification request = RestAssured.given();
+	    request.header("Content-Type", "application/x-www-form-urlencoded");
+	    request.header("Authorization", "Basic bWVyY2F0b3I6");
+	    request.header("Referer", "http://10.4.11.108");
+	    request.formParam("password", "PASSWORD%");
+	    request.formParam("username", "HONO");
+	    request.formParam("grant_type", "password");
 	    Response response = request.request(Method.POST,"/token");
 	    String resp = response.getBody().asString();
+	    System.out.println(resp);
 	    int statuscode = response.getStatusCode();
+	    String conten = response.getContentType();
 	    String token = response.jsonPath().get("access_token");
-		System.out.println(token);
+	    String token1 = response.jsonPath().get("token_type");
+	    System.out.println(token1);
+		System.out.println(conten);
 		System.out.println("statuscode" +statuscode);
+}
+}
 
-}
-}
+
